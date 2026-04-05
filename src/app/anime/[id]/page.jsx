3bc6a5362b3eafc987f9d04a5819c9a82bfe0b4d
DaffaYuza <muhammaddaffayuza@gmail.com>
2026-04-05 22:58:04 +0700

    u

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -15,24 +15,24 @@ export default async function Page({ params }) {
       </div>
       <div className="flex gap-6 pt-6 px-4">
         <div className="relative w-52 shrink-0 aspect-[2/3]">
-          <Image src={anime.data.images.webp.image_url} alt={anime.data.title} fill className="object-cover rounded-md" />
+          <Image src={anime.data.images.webp.image_url} alt={anime.data.title} fill className="object-cover rounded-lg border border-color-blue" />
         </div>
         <div className="flex flex-col gap-2">
           <div>
-            <h1 className="text-color-blue text-lg underline">Peringkat:</h1>
+            <h1 className="text-color-blue text-lg">Peringkat:</h1>
             <p className="text-color-font text-md md:text-lg">{anime.data.rank}</p>
           </div>
           <div>
-            <h1 className="text-color-blue text-lg underline">Skor:</h1>
+            <h1 className="text-color-blue text-lg">Skor:</h1>
             <p className="text-color-font text-md md:text-lg">{anime.data.score}</p>
           </div>
           <div>
-            <h1 className="text-color-blue text-lg underline">Duration:</h1>
+            <h1 className="text-color-blue text-lg">Duration:</h1>
             <p className="text-color-font text-md md:text-lg">{anime.data.duration}</p>
           </div>
           <div>
-            <h1 className="text-color-blue text-lg underline">Genres:</h1>
-            <p className="text-color-font text-md md:text-lg w-52 md:w-full">
+            <h1 className="text-color-blue text-lg">Genres:</h1>
+            <p className="text-color-font text-md md:text-lg">
               {anime.data.genres.map((genre, index) => (
                 <span key={index}>
                   {genre.name}
@@ -42,13 +42,13 @@ export default async function Page({ params }) {
             </p>
           </div>
           <div>
-            <h1 className="text-color-blue text-lg underline">Episode:</h1>
+            <h1 className="text-color-blue text-lg">Episode:</h1>
             <p className="text-color-font text-lg">{anime.data.episodes}</p>
           </div>
         </div>
       </div>
-      <div className='px-4 pt-6'>
-        <h1 className="text-xl mb-2 w-fit text-color-blue rounded font-semibold underline">Synopsis :</h1>
+      <div className="px-4 pt-6">
+        <h1 className="text-xl mb-2 w-fit text-color-blue rounded font-semibold">Synopsis :</h1>
         <p className="text-md md:text-lg text-justify text-color-font">{anime.data.synopsis}</p>
       </div>
 
